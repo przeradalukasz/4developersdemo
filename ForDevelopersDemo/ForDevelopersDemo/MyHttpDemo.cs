872b852783ff3c4fc9cdbd8b997a6e917781734d
przeradalukasz <przerada.lukasz@gmail.com>
2018-04-09 10:18:47 +0200

    Initial commit

---
+++ ForDevelopersDemo/ForDevelopersDemo/MyHttpDemo.cs
@@ -0,0 +1,31 @@
+
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+
+namespace ForDevelopersDemo
+{
+    public static class MyHttpDemo
+    {
+        [FunctionName("MyHttpDemo")]
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "saveMessage")]HttpRequest req,
+            [Queue("demomessages", Connection = "queueConn")] ICollector<string> outputMessage, TraceWriter log)
+        {
+            string requestBody = new StreamReader(req.Body).ReadToEnd();
+            Person person = JsonConvert.DeserializeObject<Person>(requestBody);
+            if (person == null || person.FirstName == null || person.LastName == null)
+            {
+                return new BadRequestObjectResult("Przekaz poprawne dane osobowe");
+            }
+
+            string message = person.FirstName + " " + person.LastName;
+            outputMessage.Add(message);
+            return new OkObjectResult($"Zapisano wiadomosc: {message}");
+        }
+    }
+
+}
